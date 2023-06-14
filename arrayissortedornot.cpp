2be3ec34c685c75d61d83110b4249f99e2d20295
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-06-14 14:13:30 +0530

    Create arrayissortedornot.cpp

---
+++ arrayissortedornot.cpp
@@ -0,0 +1,11 @@
+int n=nums.size();
+        int cnt=0;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                cnt++;
+            }
+        }
+        if(nums[n-1]>nums[0]){
+            cnt++;
+        }
+        return cnt<=1;
