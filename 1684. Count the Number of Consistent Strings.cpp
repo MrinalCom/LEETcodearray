0b6cb64fb9d4785e9c117fbe4efd27aeb7ad1039
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-08-31 20:04:08 +0530

    Create 1684. Count the Number of Consistent Strings.cpp

---
+++ 1684. Count the Number of Consistent Strings.cpp
@@ -0,0 +1,17 @@
+int c=0;
+        set<char>k(allowed.begin(),allowed.end());
+       int ans=nums.size();
+        for(auto it:nums){
+            for(int i=0;i<it.length();i++){
+                if(k.find(it[i])==k.end()){
+
+                ans--;
+                break;
+                }
+            }
+           
+
+        }
+        return ans;
+    }
+};
